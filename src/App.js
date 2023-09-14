ff547c33eb02e7503067c64de336bd425685daf6
Val Sab <valerie.sabando@gmail.com>
2023-09-14 14:37:04 +0200

    Create skeleton of project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 import React, { useEffect, useState } from 'react';
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <form
+      {{style
+          font: 'courier new'}}
+      >
+        <label for="toptext">
+          Top text:
+          <br />
+        </label>
+        <input type="text" id="toptext" name="toptext"></input>
+        <br />
+        <br />
+        <label for="bottomtext">
+          Bottom text:
+          <br />
+        </label>
+        <input type="text" id="bottomtext" name="bottomtext"></input>
+        <br />
+        <br />
+        <label for="memetemplate">
+          Meme template:
+          <br />
+        </label>
+        <input type="text" id="memetemplate" name="memetemplate"></input>
+        <br />
+        <br />
+      </form>
+      <br />
+      <button type="button">Download</button>
+    </div>
+  );
 }
