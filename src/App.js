819731a5bb53654d1261c8cd73448cad5028e232
Val Sab <valerie.sabando@gmail.com>
2023-09-16 08:40:39 +0200

    Change codes for button & memeTemplate.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { useState } from 'react';
 import BottomText from './Components/BottomText.js';
-import DownloadButton from './Components/DownloadButton.js';
-import GenerateButton from './Components/GenerateButton.js';
-import Header from './Components/Header.js';
 import MemeTemplate from './Components/MemeTemplate.js';
 import TopText from './Components/TopText.js';
 
@@ -13,7 +10,19 @@ export default function App() {
 
   return (
     <>
-      <Header />
+      <h1
+        style={{
+          fontFamily: 'Monospace',
+          fontStretch: 'ultra-expanded',
+          fontSize: '2rem',
+          color: '#6A7142',
+          marginLeft: '10px',
+          marginBottom: '10px',
+          textTransform: 'uppercase',
+        }}
+      >
+        Meme Generator
+      </h1>
       <br />
       <br />
       <TopText topText={topText} setTopText={setTopText} />
@@ -24,9 +33,46 @@ export default function App() {
         memeTemplate={memeTemplate}
         setMemeTemplate={setMemeTemplate}
       />
+      https://api.memegen.link/images/doge/such_meme/very_skill.png
+      https://api.memegen.link/images/drunk/Walk_in_a_straight_line~q/Officer,_I_can_barely_stand.png
+      https://api.memegen.link/images/ermg/ermahgerd/memes.png
       <br />
-      <GenerateButton />
-      <DownloadButton />
+      <button
+        style={{
+          fontFamily: 'Monospace',
+          fontStretch: 'ultra-expanded',
+          fontSize: '1rem',
+          fontWeight: 'lighter',
+          backgroundColor: '#c7bfc2',
+          marginLeft: '10px',
+          marginBottom: '10px',
+          textTransform: 'uppercase',
+          border: '1px solid',
+          borderRadius: '25px',
+          padding: '10px',
+        }}
+        type="button"
+      >
+        Generate
+      </button>
+      <button
+        style={{
+          fontFamily: 'Monospace',
+          fontStretch: 'ultra-expanded',
+          fontSize: '1rem',
+          fontWeight: 'lighter',
+          backgroundColor: '#c7bfc2',
+          marginLeft: '10px',
+          marginBottom: '10px',
+          textTransform: 'uppercase',
+          border: '1px solid',
+          borderRadius: '25px',
+          padding: '10px',
+        }}
+        type="button"
+      >
+        Download
+      </button>
       <br />
       <img
         src={`https://api.memegen.link/images/both/${topText}/${bottomText}.png?height=450&width=800&token=6alj86spiq9iyevbknm3`}
