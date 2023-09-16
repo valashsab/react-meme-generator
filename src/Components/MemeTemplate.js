0ed52e3b12e06bc6a803dd10ac7d9bee3c21577d
Val Sab <valerie.sabando@gmail.com>
2023-09-16 22:37:30 +0200

    Adapt component memeTemplat: object with urls

--- src/Components/MemeTemplate.js
+++ src/Components/MemeTemplate.js
@@ -1,5 +1,21 @@
 export default function MemeTemplate(props) {
-  return (
+  const memeTemplates = {
+    doge: 'https://api.memegen.link/images/doge/such_meme/very_skill.png',
+    buzz: 'https://api.memegen.link/images/buzz/memes/memes_everywhere.gif',
+    both: 'https://api.memegen.link/images/both/width_or_height/why_not_both~q.png?height=450&width=800&token=6alj86spiq9iyevbknm3',
+    drunk: 'https://api.memegen.link/images/drunk/Walk_in_a_straight_line~q/Officer,_I_can_barely_stand.png',
+    ermg: 'https://api.memegen.link/images/ermg/ermahgerd/memes.png',
+  }
+
+  const [memeTemplate, setMemeTemplate] = useState('memeTemplates.doge')
+      return (
+        if(input === 'doge') {
+          return ${props.memeTemplates.doge};
+        }
+      )
+}
+
+  {/* return (
     <form
       style={{
         fontFamily: 'Monospace',
@@ -33,3 +49,4 @@ export default function MemeTemplate(props) {
     </form>
   );
 }
+*/}
